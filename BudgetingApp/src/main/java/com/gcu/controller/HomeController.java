dd515f3583df1862a6fbfb6f38082834b9232f0e
Jack Parkison <157999365+Sonnumber5@users.noreply.github.com>
2025-02-17 23:57:32 -0500

    Add files via upload

--- BudgetingApp/src/main/java/com/gcu/controller/HomeController.java
+++ BudgetingApp/src/main/java/com/gcu/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.gcu.business.ExpensesBusinessInterface;
+import com.gcu.business.FundsBusinessInterface;
 import com.gcu.business.IncomesBusinessInterface;
 
 @Controller
@@ -17,15 +18,20 @@ public class HomeController {
 	@Autowired
 	IncomesBusinessInterface incomesBusinessInterface;
 	
+	@Autowired
+	FundsBusinessInterface fundsBusinessInterface;
+	
 	//receives a POST request, and returns to the home screen
     @GetMapping("/home")
     public String showHomePage(Model model) {
     	double expenses = expensesBusinessInterface.calculateTotalExpenses();
     	double incomes = incomesBusinessInterface.calculateTotalIncomes();
+    	double funds = fundsBusinessInterface.calculateTotalFunds();
     	
         model.addAttribute("title", "Home Page");  
         model.addAttribute("expenses", expenses);
         model.addAttribute("incomes", incomes);
+        model.addAttribute("funds", funds);
         return "home";  
     }
 }
